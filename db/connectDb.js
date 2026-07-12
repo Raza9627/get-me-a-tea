e1a4275687396910d6a157d1484cd1d02147dbcc
Raza9627 <techyhuntz@gmail.com>
2026-07-13 01:41:06 +0530

    updated version

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB= async ()=>{
     try {
-        const conn=await mongoose.connect(`mongodb://localhost:27017/chai`,{
+        const conn=await mongoose.connect(process.env.MONGODB_URI,{
         });
     } catch (error){
         console.log(error)
